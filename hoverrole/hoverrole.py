a0f887db19cfc826b277053f9ea896e39eda49fd
Blomakrans <simonb92@gmail.com>
2016-06-29 15:11:50 +0000

    Added translation from stae.is database and fixed several bugs. Fixed Latex bug that caused the highlighted word not to show up, the highlighted word is now italic in Latex.

--- hoverrole/hoverrole.py
+++ hoverrole/hoverrole.py
@@ -1,20 +1,21 @@
-# coding: utf-8
+# -*- coding: UTF-8-sig -*-
 
 # The role :hover:`word,term` provides an inline highlight of 'word' with a mouse-over 
 # translation from Icelandic to English of 'term' according to the stae.is/os database.
 # In the case of :hover:`word`. The string 'term' is assumed to be the same as 'word'
 
 # Author: Símon Böðvarsson
-# 30.05.2016
+# 29.06.2016
 
 from docutils import nodes, utils
 from docutils.parsers.rst.roles import set_classes
+import ordaskra
 
 def hover_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
     # for text input of the form: "word,term"
     try:
         [word,term] = text.split(",")
-    # If no term is provided, it is assumed to be the same as word.
+    # If no 'term' is provided, it is assumed to be the same as 'word'.
     except ValueError: 
         word=term=text
 
@@ -22,32 +23,41 @@ def hover_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
     return [node],[]
 
 def make_hover_node(word,term):
-    # Tooltip HTML code.
-    code = '<a href="http://www.stae.is/fletta/'+term+'" class="tooltip" target="_blank">'+word+u'<span><strong>Smelltu</strong> fyrir stae.is grein um hugtakið: <i>'+term+'</i></span></a>'
+    # Get English translation of Icelandic term.
+    translation = ordaskra.isTranslate(term)
+    # If no translation is found, append error message instead of translation.
+    if translation == '':
+        errorMsg = u'Ekki fannst þýðing á hugtakinu: %s' %term
+        codebit = '<a class="tooltip" target="_blank">'+word+'<span><strong>'+errorMsg+'</strong></span></a>'
+    else:
+        codebit = '<a href="http://www.stae.is/os/leita/'+unicode(translation.replace(" ","_"))+'" class="tooltip" target="_blank">'+word+'<span>en: <i>'+unicode(translation)+u'</i><br><strong>Smelltu</strong> fyrir ítarlegri þýðingu.</span></a>'
 
     # Create new hover object.
     node = hover()
-    # Add the relevant code to the object.
-    node.code = code
+    # Add the relevant codebit to the object.
+    node['code'] = codebit
+    node['word'] = word
     return node
 
 class hover(nodes.General, nodes.Element):
-    code = ''
     pass
+
+
 
 def html_hover_visit(self,node):
     # If HTML is to be generated, append the code.
-    self.body.append(node.code)
+    self.body.append(node['code'])
 
 def html_hover_depart(self,node):
     pass
 
 def tex_hover_visit(self,node):
-    # If TEX code is to be generated, do nothing.
     pass
 
 def tex_hover_depart(self,node):
-    pass
+    # If TEX code is to be generated, append 'word' in italic.
+    self.body.append("\\textit{"+node['word']+"}")
+    
 
 def setup(app):
     # Extension setup.
---
+++ hoverrole/ordaskra.py
@@ -0,0 +1,156 @@
+# -*- coding: UTF-8-sig -*-
+
+# 'ordaskra.py' and 'ordasafn.py' define a dictionary of mathematical
+# terms in Icelandic and English and functions to translate a given term from 
+# Icelandic to English or vice versa.
+# See functions: isTranslate(), enTranslate() and lookUp().
+
+# Functions extractValues() and createDictFromFile() are used to generate the
+# dictionary file 'ordasafn.py' from the file 'output'.
+
+# To generate 'ordasafn.py' from 'output' run: 
+# >> python ordaskra.py
+
+# Author: Simon Bodvarsson
+# 28.06.2016
+
+import ordasafn
+
+# Create dictionary file 'ordasafn.py' from 'output' by typing into the terminal: 
+# >> python ordaskra.py
+if __name__ == "__main__":
+	ordasafn = createDictFromFile()
+	targetfile = open('ordasafn.py','w+')
+	print >>targetfile, r'# -*- coding: UTF-8-sig -*-'
+	print >>targetfile, 'os =',
+	print >>targetfile, ordasafn
+	targetfile.close()
+
+# Look-up and translate the Icelandic searchterm 'isTerm' to English.
+# Returns only the top result and only the first 'enTerm' translation of it.
+def isTranslate(isTerm):
+	results = lookUp(isTerm,'isTerm')
+	topResult = results[0]
+	try: 
+		translation  = topResult['enTerm']
+		return translation[0]
+	except KeyError:
+		print("No results found for Icelandic term:  '%s'" %isTerm)
+		translation = ''
+	return translation
+
+# Look-up and translate the English searchterm 'enTerm' to Icelandic.
+# Returns only the top result and only the first 'isTerm' translation of it.
+def enTranslate(enTerm):	
+	results = lookUp(enTerm,'enTerm')
+	topResult = results[0]
+	try:
+		translation  = topResult['isTerm']
+		return translation[0]
+	except KeyError:
+		print("No results found for English term:  '%s'" %enTerm)
+		translation = ''
+	return translation
+
+# Look-up all relevant info for the searchterm 'sTerm' under the key 'key'.
+def lookUp(sTerm,key):
+
+	if isinstance(sTerm,str):
+		pass
+	elif isinstance(sTerm, unicode):
+		sTerm = sTerm.encode('utf-8')
+
+	results = []
+	# For each line in the dictionary 'os', find matching results and add
+	# to the 'results' list. Exact matches are added to the front of 'results'.
+	for line in ordasafn.os:
+		try: 
+			dictTerm = line[key]
+		except KeyError:
+			# Current line in 'os' does not have an entry for 'key'. It is skipped.
+			continue
+		
+		if any(term == sTerm.lower() for term in dictTerm):
+			# Exact dictionary match. Result is added to front of list of results.
+			results.insert(0,line)
+
+		if any(sTerm.lower() in term for term in dictTerm):
+			# Inexact dictionary match. Result is added to back of list of results.
+			results.append(line)
+
+	if results == []:
+		# No results found.
+		results.append({})
+
+	return results
+
+# Find and extract values from a given line into a dictionary.
+def extractValues(line):
+	# Values are seperated by TAB character. 
+	data = line.split('	')
+	if len(data) < 8:
+		return{}
+	# Put the data into a dict splitting multiple entries by ", "
+	values= {
+				'id':data[0].split(', '),
+				'translNum':data[1].split(', '),
+				'enTerm':data[2].split(', '),
+				'class':data[4].split(', '),
+				'isTerm':data[5].split(', '),
+				'synonyms':data[6].split(', '),
+				'relatedTerms':data[7].split(', '),
+			}
+	# Replace empty values with empty string.
+	for key,value in values.iteritems():
+		if len(value) == 1 and value[0].find('\\')>-1:
+			# print r"replacing '%s' for key '%s'" %(value[0],key)
+			value[0] = ""
+
+	return values
+
+# Print a given dictionary or list. Used for testing.
+def printdict(obj):
+    if type(obj) == dict:
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                print k
+                printdict(v)
+            else:
+                print '%s : %s' % (k, v)
+    elif type(obj) == list:
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                printdict(v)
+            else:
+                print v
+    else:
+        print obj
+
+# Read from file 'output' and write its content, line by line, to a list.
+# 'dictList' is a list of dict. Each line in 'output' is contained in
+# a single dict. 
+def createDictFromFile():
+	# Read the dictionary file 'osDB' which has been generated from a .dat file and write it's content, line by line, into a list.
+	dictFile = open('output')
+	dictList = dictFile.readlines()
+	# Close the dictionary file when its contents have been read.
+	dictFile.close()
+
+	# Extract all 7 values for each line.
+	# 1: Line number
+	# 2: Number of translations
+	# 3: English searchterm
+	# 4: Unused
+	# 5: Word class. Numbers 3,5 and 7 represent adjectives, nouns and verbs respectively.
+	# 6: Icelandic translation of term.
+	# 7: English synonyms
+	# 8: Related terms, e.g. for a 'See also:...'.
+	# Values are seperated by TAB-character and empty values are represented by \N.
+
+	# Replace each line in dictList with the same content reformatted into a dict of lists
+	# of the form: 
+	# {'id':[], 'translationNum':[], 'enTerm':[], 'class':[], 'isTerm':[], 'synonyms':[], 'relatedTerms':[]}
+	for index,line in enumerate(dictList):
+		dictList[index] = extractValues(line)
+
+	return dictList
